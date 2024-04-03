866ba080dead1ce8e64e0573d5888200ebc43687
leelee <leedg919@gmail.com>
2024-04-03 14:20:10 +0900

    react setting

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Contents from "../components/Contents";
+import Footer from "../components/Footer";
+
+function MainPage() {
+  return (
+    <>
+      <Header />
+      <Contents />
+      <Footer />
+    </>
+  );
+}
+
+export default MainPage;
