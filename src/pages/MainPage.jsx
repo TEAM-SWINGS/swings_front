c51524cd21ccf4a8157f1f1426b49a9e72541bb1
leelee <leedg919@gmail.com>
2024-04-23 16:36:09 +0900

    board 컴포넌트 코드 분리 진행중

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,31 @@
+import React, { useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import BoardHeader from "../components/BoardHeader";
 import Board from "../components/Board";
-import React, { useState } from "react";
+import BoardPagination from "../components/BoardPagination";
 
 function MainPage() {
+  // MainPage 컴포넌트의 상태
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const [selectedTeam, setSelectedTeam] = useState("");
+
+  // // Board 컴포넌트에서 페이지 변경 시 호출할 함수
+  // const handlePageChange = (page) => {
+  //   setCurrentPage(page);
+  // };
+
+  // // BoardHeader 컴포넌트에서 팀 선택 시 호출할 함수
+  // const handleTeamSelect = (team) => {
+  //   setSelectedTeam(team);
+  // };
 
   return (
     <>
       <Header />
+      {/* <BoardHeader currentPage={currentPage} selectedTeam={selectedTeam} handleTeamSelect={handleTeamSelect} /> */}
+      {/* <Board currentPage={currentPage} handlePageChange={handlePageChange} selectedTeam={selectedTeam} /> */}
+      {/* <BoardPagination currentPage={currentPage} handlePageChange={handlePageChange} /> */}
       <Board />
       <Footer />
     </>
